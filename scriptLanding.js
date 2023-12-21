044f081f26f86e15e17218ed84828e7df54798ae
Yosi1602 <agatayosephine@gmail.com>
2023-12-21 17:50:32 +0100

    main page initial

--- scriptLanding.js
+++ scriptLanding.js
@@ -2,6 +2,13 @@ const startButton = document.getElementById("start_button");
 
 
 startButton.addEventListener("click", () => {
-    document.getElementsByTagName("head")[3] = "<link rel='stylesheet' href='styleMain.css'/>"
-    document.body.innerHTML = "<div class='wrapper'><h2 class='question'>Will you go out with me?</h2><img class='gif' alt='gif' src='https://media.giphy.com/media/FTGah7Mx3ss04PcasF/giphy.gif'/><div class='btn-group'<button class='yes-btn'>Yes</button><button class='no-btn'>No</button></div></div><script src='scriptMain.js'></script>"
+    document.head.innerHTML = "<meta charset='UTF-8'>"+
+    "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"+
+    "<title>Do You Love Me?</title>"+
+    "<link rel='stylesheet' href='styleMain.css'/>"
+
+    document.body.innerHTML = "<div class='wrapper'><h2 class='question'>Will you go out with me?</h2>"+
+    "<img class='gif' alt='gif' src='https://media.giphy.com/media/FTGah7Mx3ss04PcasF/giphy.gif'/>"+
+    "<div class='btn-group'><button class='yes-btn'>Yes</button>"+
+    "<button class='no-btn'>No</button></div></div><script src='scriptMain.js'></script>"
 });
