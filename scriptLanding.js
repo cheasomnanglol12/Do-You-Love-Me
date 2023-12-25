b2645ea8f50a529d93ada5cf45f778dc24097824
Yosi1602 <agatayosephine@gmail.com>
2023-12-25 15:16:39 +0100

    landingPage final

--- scriptLanding.js
+++ scriptLanding.js
@@ -8,7 +8,7 @@ startButton.addEventListener("click", () => {
     "<link rel='stylesheet' href='styleMain.css'/>"
 
     document.body.innerHTML = "<div class='wrapper'><h2 class='question'>Will you go out with me?</h2>"+
-    "<img class='gif' alt='gif' src='https://media.giphy.com/media/FTGah7Mx3ss04PcasF/giphy.gif'/>"+
+    "<img class='gif' alt='gif' src='https://media.giphy.com/media/0kDdAFAELmvvFNUKim/giphy.gif'/>"+
     "<div class='btn-group'><button class='yes-btn'>Yes</button>"+
     "<button class='no-btn'>No</button></div></div><script src='scriptMain.js'></script>"
 });
