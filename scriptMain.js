19f0fdd5a549ec3e4ce43ebd281f3afc45ed2009
Yosi1602 <agatayosephine@gmail.com>
2023-12-25 15:50:54 +0100

    scriptMain typo

--- scriptMain.js
+++ scriptMain.js
@@ -4,11 +4,13 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
+    document.body.innerHTML = "<div class='wrapper'><h2 class='question'>Yay! Thank you for loving me ❤️</h2>"+
+        "<img class='gif' alt='gif' src='https://media.giphy.com/media/fHGl1MDMNkO6fOaFDF/giphy.gif'/></div><script src='scriptMain.js'></script>"
     question.innerHTML = "Yay! Thank you for loving me ❤️";
     gif.src = "https://media.giphy.com/media/fHGl1MDMNkO6fOaFDF/giphy.gif";
 });
 
-noBtn.addEventListener("click", () => {
+noBtn.addEventListener("mouseover", () => {
     gif.src ="https://media.giphy.com/media/hbOgjMOUfLdWV2Ty1j/giphy.gif";
     question.innerHTML = "You don't love me?";
     const noBtnRect = noBtn.getBoundingClientRect();
